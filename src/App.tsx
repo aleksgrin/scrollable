40506cec79a8804b1f2f98dbba72d752d36a50b6
alex-grin <aleksgrin96@gmail.com>
2020-06-13 17:56:12 +0300

    добавлено событие скролла стрелками

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Example1 from "./examples/example_1/Example_1";
 import Example2 from "./examples/example_2/Example_2";
 import Example3 from "./examples/example_3/Example_3";
 
-const startPos = 20;
-let timer: number | undefined;
 function App() {
-  const [position, setPosotion] = useState(startPos);
-  const velocity = 0.02;
-  useEffect(() => {
-    timer = window.setInterval(() => {
-      const targetPos = position + velocity * 10;
-      if (targetPos > window.innerHeight - 100) setPosotion(startPos);
-      else setPosotion(targetPos);
-    }, 10);
-    return () => {
-      window.clearInterval(timer);
-    };
-  }, [position]);
-
   return (
     <div className="App">
       <Example1 />
       <Example2 />
       <Example3 />
-      <div className="app_test" style={{ top: `${position}px` }}>
-        velocity: {velocity}
-      </div>
     </div>
   );
 }
--- src/scrollable/Scrollable.tsx
+++ src/scrollable/Scrollable.tsx
@@ -592,6 +592,40 @@ class ScrollWrap extends PureComponent<IScrollWrapProps, IState> {
     }
   };
 
+  onKeyDown = (evt: KeyboardEvent) => {
+    const { isCursorInside } = this.scrollData;
+    const { isRenderScroll } = this.state;
+    if (!isCursorInside) return;
+    evt.preventDefault();
+    const TRANSLATE_SMALL = 50;
+    const TRANSLATE_BIG = 150;
+    const isArrowKey = [
+      "ArrowDown",
+      "ArrowUp",
+      "ArrowLeft",
+      "ArrowRight",
+    ].includes(evt.key);
+    const TRANSLATE_VALUE = evt.repeat ? TRANSLATE_BIG : TRANSLATE_SMALL;
+
+    if (isArrowKey) this.setState({ transition: "" });
+    if (isRenderScroll.y && evt.key === "ArrowDown") {
+      const nextOffset = this.state.offset.y - TRANSLATE_VALUE;
+      this.setScroll(nextOffset, "y");
+    }
+    if (isRenderScroll.y && evt.key === "ArrowUp") {
+      const nextOffset = this.state.offset.y + TRANSLATE_VALUE;
+      this.setScroll(nextOffset, "y");
+    }
+    if (isRenderScroll.x && evt.key === "ArrowLeft") {
+      const nextOffset = this.state.offset.x + TRANSLATE_VALUE;
+      this.setScroll(nextOffset, "x");
+    }
+    if (isRenderScroll.x && evt.key === "ArrowRight") {
+      const nextOffset = this.state.offset.x - TRANSLATE_VALUE;
+      this.setScroll(nextOffset, "x");
+    }
+  };
+
   initEventListeners = () => {
     window.document.addEventListener("wheel", this.onDocumentWheel, {
       passive: false,
@@ -600,6 +634,7 @@ class ScrollWrap extends PureComponent<IScrollWrapProps, IState> {
       passive: false,
     });
     window.addEventListener("resize", this.onWindowResize);
+    window.addEventListener("keydown", this.onKeyDown);
   };
 
   destroyEventListeners = () => {
